def65c8f69e6fcadf8f805f340f67b160b5f281a
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-06-02 08:11:31 +0530

    Update roboshop-infra/user-infra/

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+output "VPC_ID" {
+  value = module.vpc.VPC_ID
+}
+
+output "VPC_CIDR" {
+  value =  module.vpc.VPC_CIDR
+}
+
+output "SUBNET_IDS" {
+  value =  module.vpc.SUBNET_IDS
+}
+
+output "SUBNET_CIDR" {
+  value = a module.vpc.SUBNET_CIDR
+}
+
