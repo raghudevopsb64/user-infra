d0cd1d7f4c725644e511006dad01ad88d6a5fe07
Raghu K <raghuk.vit@gmail.com>
2022-06-18 08:21:42 +0530

    Update roboshop-infra/user-infra/

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ module "app" {
   NODE_TYPE               = var.EC2_NODE_TYPE
   PORT                    = var.PORT
   WORKSTATION_IP          = var.WORKSTATION_IP
-  VPC_ACCESS_TO_ALB       = [module.vpc.VPC_CIDR]
+  VPC_ACCESS_TO_ALB       = [module.vpc.VPC_CIDR, "${module.vpc.NGW_PRIVATE_IP}/32", "10.10.5.0/24"]
   DOCDB_ENDPOINT          = module.documentdb.DOCDB_ENDPOINT
   IAM_POLICY_CREATE       = true
   PRIVATE_HOSTED_ZONE_ID  = module.vpc.PRIVATE_HOSTED_ZONE_ID
